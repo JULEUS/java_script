87e001c55e2121d44ea03d97cfc7bef82c10286e
Julia Demchenko <juleus@mail.ru>
2016-05-26 19:24:08 +0300

    Add hometask

--- 2-11.js
+++ 2-11.js
@@ -20,8 +20,10 @@ if (enterNum > 0){
 	alert('1 The num is bigger than zero');
 } else if (enterNum < 0){
 	alert('-1 Tne num is less than zero');
-} else {
+} else if (enterNum = 0){
 	alert('0 Is your num zero?!');
+} else {
+	alert('It is not a number');
 }
 
 /* 2-12 #3 */
@@ -46,14 +48,24 @@ if (login == 'admin'){
 var a = prompt('Enter first num', ''), 
 	b = prompt('Enter second num', '');
 var summ = (+a + +b < 4) ? 'Not a much!' :
-	'Too much!';
+	'Too much or it is not a number!';
 alert (summ);
 
 /* 2-12 #5 */
 var login = prompt('Enter your login', 'john, boss ...');
-var message = (login == 'john') ? 'Hi!' :
-	message = (login == 'boss') ? 'Hello!' :
+var message = (login == 'john') ? 'Hi, John!' :
+	message = (login == 'boss') ? 'Hello, Boss!' :
 	message = (login == '') ? 'No login' :
 	'error';
 
 alert(message);
+
+/* 2-13 #1 */
+var age = prompt("Enter a number");
+if (age >= 14 && age <= 90){
+	alert('Good number');
+} else if (age <14 || age > 90){
+	alert('Ooops!Too small or too big number');
+} else {
+	alert('It is not a num!');
+}
